22873531294e564b968e4cee08c6681074c25698
Arturo Ortega <arturo6111@ahoo.com>
2024-08-31 12:34:27 -0700

    Implement small guessing game in Rust

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,37 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    let mut guesses = 0;
+
+    loop {
+        println!("Input your guess: ");
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess).expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue
+        };
+
+        guesses += 1;
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+    println!("You won in {guesses} guesses!");
+}
